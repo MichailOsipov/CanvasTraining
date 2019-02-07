2ae438e520169f39ca77b0f851253f14636f2aa2
Mikhail Osipov <Mikhail_Osipov@epam.com>
2019-02-07 18:27:45 +0300

    Added more canvas stuff.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 const body = document.querySelector('body');
 
-const draw = () => {
+const initContextJob = () => {
     const canvas = document.getElementById('canvas');
     if (!canvas.getContext) {
         return;
@@ -226,6 +226,219 @@ const draw = () => {
     //     ctx.lineTo(170, 15);
     //     ctx.stroke();
     // }, false);
+
+    // ctx.fillRect(0, 0, 150, 150); // Draw a rectangle with default settings
+    // ctx.save(); // Save the default state
+
+    // ctx.fillStyle = '#09F'; // Make changes to the settings
+    // ctx.fillRect(15, 15, 120, 120); // Draw a rectangle with new settings
+
+    // ctx.save(); // Save the current state
+    // ctx.fillStyle = '#FFF'; // Make changes to the settings
+    // ctx.globalAlpha = 0.5;
+    // ctx.fillRect(30, 30, 90, 90); // Draw a rectangle with new settings
+
+    // ctx.restore(); // Restore previous state
+    // ctx.fillRect(45, 45, 60, 60); // Draw a rectangle with restored settings
+
+    // ctx.restore(); // Restore original state
+    // ctx.fillRect(60, 60, 30, 30); // Draw a rectangle with restored settings
+
+
+    // for (let i = 0; i < 3; i += 1) {
+    //     for (let j = 0; j < 3; j += 1) {
+    //         ctx.save();
+    //         ctx.fillStyle = `rgb(${51 * i},${255 - (51 * i)}, 255)`;
+    //         ctx.translate(10 + (j * 50), 10 + (i * 50));
+    //         ctx.fillRect(0, 0, 25, 25);
+    //         ctx.restore();
+    //     }
+    // }
+
+    // ctx.save();
+    // // blue rect
+    // ctx.fillStyle = '#0095DD';
+    // ctx.fillRect(30, 30, 100, 100);
+    // ctx.rotate((Math.PI / 180) * 25);
+    // // // grey rect
+    // ctx.fillStyle = '#4D4E53';
+    // ctx.fillRect(30, 30, 100, 100);
+    // ctx.restore();
+
+    // // // right rectangles, rotate from rectangle center
+    // // // draw blue rect
+    // ctx.fillStyle = '#0095DD';
+    // ctx.fillRect(150, 30, 100, 100);
+
+    // ctx.translate(200, 80); // translate to rectangle center
+    // //                         // x = x + 0.5 * width
+    // //                         // y = y + 0.5 * height
+    // ctx.rotate((Math.PI / 180) * 25); // rotate
+    // ctx.translate(-200, -80); // translate back
+
+    // // // draw grey rect
+    // ctx.fillStyle = '#4D4E53';
+    // ctx.fillRect(150, 30, 100, 100);
+
+
+    // ctx.save();
+    // ctx.scale(10, 3);
+    // ctx.fillRect(1, 10, 10, 10);
+    // ctx.restore();
+
+    // // mirror horizontally
+    // ctx.scale(-1, 1);
+    // ctx.font = '48px serif';
+    // ctx.fillText('MDN', -135, 120);
+
+
+    // const sin = Math.sin(Math.PI / 6);
+    // const cos = Math.cos(Math.PI / 6);
+    // ctx.translate(100, 100);
+    // let c = 0;
+    // for (let i = 0; i <= 12; i += 1) {
+    //     c = Math.floor((255 / 12) * i);
+    //     ctx.fillStyle = `rgb(${c},${c},${c})`;
+    //     ctx.fillRect(0, 0, 100, 10);
+    //     ctx.transform(cos, sin, -sin, cos, 0, 0);
+    // }
+
+    // ctx.setTransform(-1, 0, 0, 1, 100, 100);
+    // ctx.fillStyle = 'rgba(255, 128, 255, 0.5)';
+    // ctx.fillRect(0, 50, 100, 100);
+
+
+    // function drawStar(ctx, r) {
+    //     ctx.save();
+    //     ctx.beginPath();
+    //     ctx.moveTo(r, 0);
+    //     for (let i = 0; i < 9; i += 1) {
+    //         ctx.rotate(Math.PI / 5);
+    //         if (i % 2 === 0) {
+    //             ctx.lineTo((r / 0.525731) * 0.200811, 0);
+    //         } else {
+    //             ctx.lineTo(r, 0);
+    //         }
+    //     }
+    //     ctx.closePath();
+    //     ctx.fill();
+    //     ctx.restore();
+    // }
+    // ctx.fillRect(0, 0, 150, 150);
+    // ctx.translate(75, 75);
+
+    // // Create a circular clipping path
+    // ctx.beginPath();
+    // ctx.arc(0, 0, 60, 0, Math.PI * 2, true);
+    // ctx.clip();
+
+    // // draw background
+    // const lingrad = ctx.createLinearGradient(0, -75, 0, 75);
+    // lingrad.addColorStop(0, '#232256');
+    // lingrad.addColorStop(1, '#143778');
+
+    // ctx.fillStyle = lingrad;
+    // ctx.fillRect(-75, -75, 150, 150);
+
+    // // draw stars
+    // for (let j = 1; j < 50; j += 1) {
+    //     ctx.save();
+    //     ctx.fillStyle = '#fff';
+    //     ctx.translate(75 - Math.floor(Math.random() * 150), 75 - Math.floor(Math.random() * 150));
+    //     drawStar(ctx, Math.floor(Math.random() * 4) + 2);
+    //     ctx.restore();
+    // }
+
+
+    // const sun = new Image();
+    // const moon = new Image();
+    // const earth = new Image();
+    // function init() {
+    //     sun.src = 'https://mdn.mozillademos.org/files/1456/Canvas_sun.png';
+    //     moon.src = 'https://mdn.mozillademos.org/files/1443/Canvas_moon.png';
+    //     earth.src = 'https://mdn.mozillademos.org/files/1429/Canvas_earth.png';
+    //     requestAnimationFrame(draw);
+    // }
+
+    // function draw() {
+    //     ctx.globalCompositeOperation = 'destination-over';
+    //     ctx.clearRect(0, 0, 300, 300); // clear canvas
+
+    //     ctx.fillStyle = 'rgba(0, 0, 0, 0.4)';
+    //     ctx.strokeStyle = 'rgba(0, 153, 255, 0.4)';
+    //     ctx.save();
+    //     ctx.translate(150, 150);
+
+    //     // Earth
+    //     const time = new Date();
+    //     ctx.rotate((((2 * Math.PI) / 60) * time.getSeconds()) + (((2 * Math.PI) / 60000) * time.getMilliseconds()));
+    //     ctx.translate(105, 0);
+    //     ctx.fillRect(0, -12, 40, 24); // Shadow
+    //     ctx.drawImage(earth, -12, -12);
+
+    //     // Moon
+    //     ctx.save();
+    //     ctx.rotate((((2 * Math.PI) / 6) * time.getSeconds()) + (((2 * Math.PI) / 6000) * time.getMilliseconds()));
+    //     ctx.translate(0, 28.5);
+    //     ctx.drawImage(moon, -3.5, -3.5);
+    //     ctx.restore();
+
+    //     ctx.restore();
+
+    //     ctx.beginPath();
+    //     ctx.arc(150, 150, 105, 0, Math.PI * 2, false); // Earth orbit
+    //     ctx.stroke();
+
+    //     ctx.drawImage(sun, 0, 0, 300, 300);
+
+    //     window.requestAnimationFrame(draw);
+    // }
+
+    // init();
+
+    // let raf;
+    // const ball = {
+    //     x: 100,
+    //     y: 100,
+    //     vx: 5,
+    //     vy: 2,
+    //     radius: 25,
+    //     color: 'blue',
+    //     draw() {
+    //         ctx.beginPath();
+    //         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+    //         ctx.closePath();
+    //         ctx.fillStyle = this.color;
+    //         ctx.fill();
+    //     }
+    // };
+
+    // function draw() {
+    //     if (ball.y + ball.vy > canvas.height || ball.y + ball.vy < 0) {
+    //         ball.vy = -ball.vy;
+    //     }
+    //     if (ball.x + ball.vx > canvas.width || ball.x + ball.vx < 0) {
+    //         ball.vx = -ball.vx;
+    //     }
+    //     ball.vy *= 0.99;
+    //     ball.vy += 0.25;
+    //     ctx.fillStyle = 'rgba(255, 255, 255, 0.3)';
+    //     ctx.fillRect(0, 0, canvas.width, canvas.height);
+    //     ball.draw();
+    //     ball.x += ball.vx;
+    //     ball.y += ball.vy;
+    //     raf = window.requestAnimationFrame(draw);
+    // }
+
+    // canvas.addEventListener('mouseover', () => {
+    //     raf = requestAnimationFrame(draw);
+    // });
+
+    // canvas.addEventListener('mouseout', () => {
+    //     window.cancelAnimationFrame(raf);
+    // });
+
+    // ball.draw();
 };
 
-body.onload = draw;
+body.onload = initContextJob;
